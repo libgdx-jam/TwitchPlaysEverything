b5e12a4e9130112ef6a4f27c547c894708a2522f
Alex Place <alex.place.7@gmail.com>
2015-10-16 19:18:34 -0400

    initial project setup

---
+++ TwitchPlaysEverything/src/co/porkopolis/twitchplays/Main.java
@@ -0,0 +1,14 @@
+package co.porkopolis.twitchplays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.print();
+	}
+
+	public void print() {
+		System.out.println("Hello World!");
+	}
+
+}
