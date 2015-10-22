931c1adb01966bdff846e0a06201856310cd4a8a
Alex Place <alex.place.7@gmail.com>
2015-10-22 00:43:04 -0400

    Added test project to repo for nightly

---
+++ TwitchTestsEverything/test/test/RobotTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+public class RobotTest {
+	public static void main(String[] args) throws AWTException {
+		Scanner scanner = new Scanner(System.in);
+		String input = "";
+		Robot robot = new Robot();
+
+		while (true) {
+			System.out.println("type a word: ");
+			input = scanner.next();
+
+			switch (input) {
+			case "up":
+				robot.keyPress(KeyEvent.VK_NUMPAD8);
+				break;
+			case "down":
+				robot.keyPress(KeyEvent.VK_NUMPAD2);
+				break;
+			case "left":
+				robot.keyPress(KeyEvent.VK_NUMPAD4);
+				break;
+			case "right":
+				robot.keyPress(KeyEvent.VK_NUMPAD6);
+				break;
+			case "a":
+				robot.keyPress(KeyEvent.VK_NUMPAD7);
+				break;
+			case "b":
+				robot.keyPress(KeyEvent.VK_NUMPAD9);
+				break;
+			case "start":
+				robot.keyPress(KeyEvent.VK_NUMPAD1);
+				break;
+			case "select":
+				robot.keyPress(KeyEvent.VK_NUMPAD3);
+				break;
+			}
+			
+		}
+	}
+}
