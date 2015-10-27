6dd30aead13571e91e1841c5727cfc7c5ba857ed
Alex Place <alex.place.7@gmail.com>
2015-10-26 23:50:57 -0400

    Added theme and icon

--- TwitchTestsEverything/test/test/MainApp.java
+++ TwitchTestsEverything/test/test/MainApp.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -54,6 +55,8 @@ public class MainApp extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AddressApp");
+
+		this.primaryStage.getIcons().add(new Image("file:images/icon.png"));
 
 		initRootLayout();
 
